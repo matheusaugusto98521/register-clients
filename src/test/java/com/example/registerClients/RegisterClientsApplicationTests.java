a6c88b65caff692dd468ef9ae86df18722fc22e9
Matheus Augusto <matheusferreirasoares1234@gmail.com>
2024-09-04 14:49:10 -0300

    first commit

---
+++ src/test/java/com/example/registerClients/RegisterClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
